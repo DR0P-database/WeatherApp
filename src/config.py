7f4d87b3315ceaa8635d17f5a2351fa2173a899e
DR0P-database(MacBook Air) <dropdatabase.py@gmail.com>
2024-03-28 11:37:51 +0300

    Add error_page

--- src/config.py
+++ src/config.py
@@ -1,1 +1,3 @@
 SECRET_KEY = "SECRET_KEY"
+API_KEY = 'd0e6fe21ae9307ae0dced366422ee948'
+ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather'
--- src/main.py
+++ src/main.py
@@ -1,16 +1,54 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, abort, flash
 import flask
+import requests
 from config import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
+params = {'q': 'Moscow', 'appid': API_KEY, 'units': 'metric'}
+
+
+def select_icon(clouds):
+    match (clouds):
+        case 'Clouds':
+            return "static/img/clouds.png"
+        case 'Clear':
+            return "static/img/sun.png"
+        case 'Rain':
+            return "static/img/rainy.png"
+        case 'Mist':
+            return "static/img/mist.png"
+        case 'Snow':
+            return "static/img/snow.png"
+        case 'Haze':
+            return "static/img/haze.png"
+        case _:
+            return "static/img/sun.png"
+
+
+def get_weather(params):
+    answ = requests.get(ENDPOINT, params=params)
+    if answ:
+        data = answ.json()
+        data["weather"][0]['icon'] = select_icon(data["weather"][0]['main'])
+        return data
+
+    flash('Bad request', 'error')
+    abort(404)
 
 
 @app.route('/', methods=["POST", "GET"])
 def index():
     if request.method == 'POST':
-        city = request.form['city']
-    return render_template('index.html')
+        params['q'] = request.form['city']  # для вызова след requests
+        return redirect(url_for('index', data=get_weather(params)))
+
+    return render_template('index.html', data=get_weather(params))
+
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('page_404.html')
 
 
 if __name__ == "__main__":
